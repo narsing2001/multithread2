f92e3042a1ab52bc221001e75f027e7f2b6d61cf
Narsing Patil <narsingpatil205@gmail.com>
2025-11-08 15:10:27 +0530

    main thread deadlock programme

---
+++ MainThreadDeadLock.java
@@ -0,0 +1,29 @@
+// Java program to demonstrate deadlock using Main thread
+
+public class MainThreadDeadLock {
+
+  // Main driver method
+  public static void main(String[] args) {
+
+    // Try block to check for exceptions
+    try {
+
+      
+      System.out.println("Entering into Deadlock");
+
+      // Joining the current thread
+      Thread.currentThread().join();
+
+      // This statement will never execute
+      System.out.println("This statement will never execute");
+    }
+
+    // Catch block to handle the exceptions
+    catch (InterruptedException e) {
+
+      // Display the exception along with line number
+      // using printStackTrace() method
+      e.printStackTrace();
+    }
+  }
+}
